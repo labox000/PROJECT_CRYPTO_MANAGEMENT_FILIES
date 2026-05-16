d9c927884930caa28c2e103329db2546c6fe60d6
anasB566 <anasbounouar098@gmail.com>
2026-05-16 10:22:27 +0200

    db file for files , object file in objects.py

--- objects.py
+++ objects.py
@@ -44,3 +44,70 @@ class User:
         # default to now if not provided (matches DB DEFAULT CURRENT_TIMESTAMP concept)
         self.created_at = created_at or datetime.now()
         self.updated_at = updated_at or datetime.now()
+
+
+from datetime import datetime
+
+
+class File:
+
+    def __init__(
+        self,
+        file_name: str,
+        user_id: str,
+        file_hash: str,
+        encryption_state: str,
+        encryption_algorithm=None,
+        encrypted_key=None,
+        created_at=None,
+        updated_at=None
+    ):
+
+        self.user_id = user_id
+
+        self.file_name = file_name
+        self.file_hash = file_hash
+
+        self.encryption_state = encryption_state
+        self.encryption_algorithm = encryption_algorithm
+
+        self.encrypted_key = encrypted_key
+
+        self.created_at = created_at or datetime.now()
+        self.updated_at = updated_at or datetime.now()
+
+    def update_hash(self, new_hash):
+        self.file_hash = new_hash
+        self.updated_at = datetime.now()
+
+    def set_encryption(
+        self,
+        state: str,
+        algorithm=None,
+        encrypted_key=None
+    ):
+
+        self.encryption_state = state
+        self.encryption_algorithm = algorithm
+        self.encrypted_key = encrypted_key
+
+        self.updated_at = datetime.now()
+
+    def rename_file(self, new_name):
+        self.file_name = new_name
+        self.updated_at = datetime.now()
+
+    def display_info(self):
+
+        
+        print(f"File Name: {self.file_name}")
+        print(f"User ID: {self.user_id}")
+
+
+        print(f"Hash: {self.file_hash}")
+
+        print(f"Encryption State: {self.encryption_state}")
+        print(f"Encryption Algorithm: {self.encryption_algorithm}")
+
+        print(f"Created At: {self.created_at}")
+        print(f"Updated At: {self.updated_at}")
